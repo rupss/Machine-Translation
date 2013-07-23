c9e6d75b62714af7ea97b9a8cbd7e93b98d4bd5d
Rupa Shankar <rupsshankar@gmail.com>
2013-07-22 18:25:43 -0700

    added main methods

--- translate.py
+++ translate.py
@@ -1,27 +1,31 @@
 import re
 
-spanishFile = open('SpanishText.txt', 'r')
-translated = open('translated.txt', 'w')
-dictionary = dict()
-dictFile = open('spanishDictionary.txt', 'r')
+def main(): 
+    spanishFile = open('SpanishText.txt', 'r')
+    translated = open('translated.txt', 'w')
+    dictionary = dict()
+    dictFile = open('spanishDictionary.txt', 'r')
 
-for line in dictFile.readlines(): 
-	print line
-	words = line.split()
-	span = words[0]
-	eng = ''
-	for i in range(1, len(words)): 
-		eng += words[i] + ' '
-	eng = eng.strip()
-	dictionary[span] = eng
+    for line in dictFile.readlines(): 
+        print line
+        words = line.split()
+        span = words[0]
+        eng = ''
+        for i in range(1, len(words)): 
+            eng += words[i] + ' '
+        eng = eng.strip()
+        dictionary[span] = eng
 
-for line in spanishFile.readlines(): 
-	for word in line.split(): 
-		w = word.lower()
-		w = re.sub('[.?!",]', '', w)
-		print w
-		translated.write(dictionary[w] + ' ')
-	translated.write('\n')
+    for line in spanishFile.readlines(): 
+        for word in line.split(): 
+            w = word.lower()
+            w = re.sub('[.?!",]', '', w)
+            print w
+            translated.write(dictionary[w] + ' ')
+        translated.write('\n')
+
+if __name__ == "__main__":
+    main()
 
 
 
